36cd092525c52de99c8ea5dd9e8bb71991c06b87
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-09-03 15:50:57 +0500

    Dashboard page & logout functionality completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from 'react-hot-toast';
 import { useAuthStore } from "./store/authStore";
 import { useEffect } from "react";
 import DashboardPage from "./pages/DashboardPage";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 // redirect authenticated users to home page
 const RedirectAuthenticatedUser = ({ children }) => {
@@ -38,14 +39,13 @@ const ProtectedRoute = ({ children }) => {
 }
 
 function App() {
-  const { checkAuth, isAuthenticated, user } = useAuthStore();
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log("isAuthenticated", isAuthenticated);
-  console.log("user", user);
+  if (isCheckingAuth) return <LoadingSpinner />
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
